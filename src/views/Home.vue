21d26d329d0a002f55b11f3250aa4c7fc60aeab8
FirmanDwiP <firman.informatics@gmail.com>
2020-04-01 12:55:12 +0700

    front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="hello">
+
+    <div class="container" style="margin-top: 50px;">
+      <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <h1 class="text-center">URL Singkat</h1>
+            <form action="/shortUrls" method="POST" class="my-4 form-inline">
+              <label for="fullUrl" class="sr-only">Url</label>
+              <input required placeholder="Masukkan Url" type="url" name="fullUrl" id="fullUrl" class="form-control col mr-2">
+              <button class="btn btn-primary" type="submit">Pendekkan</button>
+            </form>
+            
+            <table class="table table-striped table-responsive text-center" style="margin:auto; text-align: center;">
+              <thead>
+                <tr>
+                  <th>URL Asli</th>
+                  <th>URL Pendek</th>
+                  <th>Jumlah Click</th>
+                </tr>
+              </thead>
+              <tbody>
+                <% shortUrls.forEach(shortUrl => { %>
+                  <tr>
+                    <td><a href="<%= shortUrl.full %>"><%= shortUrl.full %></a></td>
+                    <td><a href="<%= shortUrl.short %>"><%= shortUrl.short %></a></td>
+                    <td><%= shortUrl.clicks %></td>
+                  </tr>
+                <% }) %>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-md-2"></div>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from 'axios';
+const baseURL = 'http://localhost:4000/';
+// const axios = require('axios');
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data () {
+    return {
+      todos: [],
+      id: '',
+      taskname: '',
+      isEdit: false
+    }
+  },
+  mounted () {
+    this.getTasks()
+  },
+  methods: {
+    getTasks () {
+      axios({ method: 'GET', baseURL }).then(
+        result => {
+          console.log(result.data)
+          this.todos = result.data
+        },
+        error => {
+          console.error(error)
+        }
+      )
+    }
   }
-};
-</script>
+}
+    </script>
